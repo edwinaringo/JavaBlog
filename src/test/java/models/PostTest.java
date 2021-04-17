979d024a7a8451c195596dac7ca83e9505a3e924
edwinaringo <tedaringo628@gmail.com>
2021-04-17 19:25:58 +0300

    Creating and AfterEach test to check intatntiation

--- src/test/java/models/PostTest.java
+++ src/test/java/models/PostTest.java
@@ -2,6 +2,7 @@ package models;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,6 +10,13 @@ class PostTest {
 
     @BeforeEach
     void setUp() {
+    }
+
+    @Test
+    void NewPostObjectGetsCorrectlyCreated_true()throws Exception {
+        Post post = new Post("Day 1: Intro");
+        assertEquals(true,post instanceof Post);
+
     }
 
     @AfterEach
